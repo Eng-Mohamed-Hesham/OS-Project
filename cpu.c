73e2db315d3170b04863331831ee1473cae312dd
mostafa-shoman <mustafa196043@feng.bu.edu.eg>
2022-12-26 18:37:54 +0200

    Edit sjf

--- cpu.c
+++ cpu.c
@@ -9,13 +9,55 @@ struct processes
     int waiting_time;
     int remaining_time;
     int leave_time;
+    int pid;
 };
 
 struct times
 {
     int p, art, but, wtt, tat, rnt;
 };
+void print_gantt_chart(struct processes p[], int n)
+{
+    
+    int i, j;
+    int last = p[n-1].burst_time + ( n== 1 ? 0 : p[n-1].waiting_time);
+    // printing top bar
+    printf(" ");
+    for(i=0; i<n; i++) {
+        for(j=0; j<p[i].burst_time; j++) printf("--");
+        printf(" ");
+    }
+    printf("\n|");
+    // middle position
+    for(i=0; i<n; i++) {
+        for(j=0; j<p[i].burst_time-1; j++) printf(" ");
+        printf("p%d", p[i].pid);
+        for(j=0; j<p[i].burst_time-1; j++) printf(" ");
+        printf("|");
+    }
+    printf("\n ");
+    // bottom bar
+    for(i=0; i<n; i++) {
+        for(j=0; j<p[i].burst_time; j++) printf("--");
+        printf(" ");
+    }
+    printf("\n");
 
+    // printing waiting time
+    int minus = 0;
+    for(i=0; i<n; i++) {
+        if(p[i].waiting_time>9) printf(" ");
+        printf("%d", p[i].waiting_time);
+        if(p[i+1].waiting_time>9){
+          minus = 1;
+        }
+        if(i+1 == n )  if (last>9) minus = 1;
+        for(j=0; j<p[i].burst_time-minus; j++) printf("  ");
+
+    }
+    if(last>9) printf(" ");
+    printf("%d\n", last);
+}
 
 void fcfs()
 {
@@ -39,6 +81,7 @@ void fcfs()
     int turn_time = 0;
     for (int i = 0; i < no_of_processes; i++)
     {
+        
         // printf(" %d \n",wait_time);
         p[i].waiting_time = wait_time;
         p[i].turnaround_time = wait_time + p[i].burst_time;
@@ -117,7 +160,7 @@ void sort_rem(struct processes *p, int np)
 */
 
 
-void nonpreemptive()
+void sjf()
 {
     int no_of_processes;
     printf("\nShortest Job First Algorithm\n");
@@ -139,7 +182,8 @@ void nonpreemptive()
     int wait_time = 0;
     int turn_time = 0;
     for (int i = 0; i < no_of_processes; i++)
-    {
+    {   
+        p[i].pid = i+1;
         // printf(" %d \n",wait_time);
         p[i].waiting_time = wait_time;
         p[i].turnaround_time = wait_time + p[i].burst_time;
@@ -160,91 +204,8 @@ void nonpreemptive()
     float avg_turn = turn_time / no_of_processes;
     printf("Average waiting time : %f \n", avg_wait);
     printf("Average turnaround time : %f \n", avg_turn);
-}
-
-
-void preemptive()
-{
-    int no_of_processes;
-    printf("\nPreemptive Shortest Job First Algorithm\n");
-    printf("Note -\n1. Arrival Time of at least on process should be 0\n2. CPU should never be idle\n");
-    printf("Enter number of processes:  ");
-    scanf("%d", &no_of_processes);
-    printf("############################\n");
-    struct processes *p = malloc(sizeof(struct processes) * no_of_processes);
-    for (int i = 0; i < no_of_processes; i++)
-    {
-        printf("Enter Burst time of p[%d]:  ", i);
-        scanf("%d", &p[i].burst_time);
-        printf("Enter arrival time of p[%d]:  ", i);
-        scanf("%d", &p[i].arrival_time);
-        p[i].process_id = i;
-        printf("############################\n");
-    }
-    int shortest = 0;
-    int complete = 0; // no of completed processes
-    int finish = 0;
-    int minimum = 100;
-    int wait_time = 0;
-    int turn_time = 0;
-    int t = 0;     // cpu current time
-    int check = 0; // checking if shortest time exists
-    for (int i = 0; i < no_of_processes; i++)
-        p[i].remaining_time = p[i].burst_time;
-
-    while (complete != no_of_processes)
-    {
-        for (int j = 0; j < no_of_processes; j++)
-        {
-            if ((p[j].arrival_time <= t) && (p[j].remaining_time < minimum) && (p[j].remaining_time > 0))
-            {
-                minimum = p[j].remaining_time;
-                shortest = j;
-                check = 1;
-            }
-        }
-        if (check == 0)
-        {
-            t++;
-            continue;
-        }
-
-        p[shortest].remaining_time -= 1;
-        minimum = p[shortest].remaining_time;
-        if (minimum == 0)
-            minimum = 100;
-        if (p[shortest].remaining_time == 0)
-        {
-            complete++;
-            check = 0;
-            finish = t + 1;
-            p[shortest].waiting_time = finish - p[shortest].burst_time - p[shortest].arrival_time;
-
-            if (p[shortest].waiting_time < 0)
-                p[shortest].waiting_time = 0;
-        }
-
-        t++;
-    }
-    for (int i = 0; i < no_of_processes; i++)
-        p[i].turnaround_time = p[i].burst_time + p[i].waiting_time;
-
-    for (int i = 0; i < no_of_processes; i++)
-    {
-        turn_time += p[i].turnaround_time;
-        wait_time += p[i].waiting_time;
-    }
-    printf("\t ProcessId \t\tArrival \t\tBurst \t\t Turnaround \t\t Waiting \n");
-
-    for (int i = 0; i < no_of_processes; i++)
-    {
-        printf("\t %d \t\t\t %d \t\t\t %d \t\t\t %d \t\t\t %d \n", p[i].process_id, p[i].arrival_time, p[i].burst_time, p[i].turnaround_time, p[i].waiting_time);
-    }
-
-    float avg_wait = wait_time / no_of_processes;
-    float avg_turn = turn_time / no_of_processes;
-    printf("Average waiting time : %f \n", avg_wait);
-    printf("Average turnaround time : %f \n", avg_turn);
+    print_gantt_chart(p, no_of_processes);
+    printf("##########################################################################################################\n");
 }
 
 void roundRobin()
@@ -329,10 +290,9 @@ void main()
     {
         printf("\t\t Main Menu \n");
         printf("\t 1. First Come First Serve(FCFS)\n");
-        printf("\t 2. SJF (preemptive)\n");
-        printf("\t 3. SJF (Non-Preemptive)\n");
-        printf("\t 4. Round Robin\n");
-        printf("\t 5. Exit\n");
+        printf("\t 2. SJF\n");
+        printf("\t 3. Round Robin\n");
+        printf("\t 4. Exit\n");
         printf("Enter choice:  ");
         scanf("%d", &choice);
         switch (choice)
@@ -341,12 +301,9 @@ void main()
             fcfs();
             break;
         case 2:
-            preemptive();
+            sjf();
             break;
         case 3:
-            nonpreemptive();
-            break;
-        case 4:
             roundRobin();
             break;
         }
